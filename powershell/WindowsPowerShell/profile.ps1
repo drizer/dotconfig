70b2ff174d130a4376871773a87dcebc67fee2c8
Derek Mall <drizer76@gmail.com>
2016-05-01 23:11:16 +1000

    Add powershell profile

--- powershell/WindowsPowerShell/profile.ps1
+++ powershell/WindowsPowerShell/profile.ps1
@@ -1,3 +1,4 @@
+#C:\Users\drizer\Documents\WindowsPowerShell\profile.ps1
 set-alias python35 E:\Programs\venv\py35\Scripts\Activate.ps1
 set-alias python27 E:\Programs\venv\py27\Scripts\activate.ps1
 set-alias scons E:\Programs\venv\py27\Scripts\scons.bat
