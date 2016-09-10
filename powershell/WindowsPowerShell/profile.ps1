e6fff5a0d7c8dd324e2575f5665f668cad9d5fdf
Derek Mall <drizer76@gmail.com>
2016-09-10 16:41:37 +1000

    Update profile.ps1
    checksum fuctions md5/sha256

--- powershell/WindowsPowerShell/profile.ps1
+++ powershell/WindowsPowerShell/profile.ps1
@@ -2,3 +2,22 @@
 set-alias python35 E:\Programs\venv\py35\Scripts\Activate.ps1
 set-alias python27 E:\Programs\venv\py27\Scripts\activate.ps1
 set-alias scons E:\Programs\venv\py27\Scripts\scons.bat
+
+#checksum fuctions
+function MD5Sum([string]$sFile)
+{
+    $oMD5=[System.Security.Cryptography.MD5]::Create()
+    $oStream=[System.IO.File]::OpenRead($sFile)
+    $sMD5=[System.BitConverter]::ToString($oMD5.ComputeHash($oStream)).Replace("-","").ToLower()
+    $oStream.Close()
+    return($sMD5)
+}
+
+function sha256sum([string]$sFile)
+{
+    $oSHA256=[System.Security.Cryptography.SHA256]::Create()
+    $oStream=[System.IO.File]::OpenRead($sFile)
+    $sSHA256=[System.BitConverter]::ToString($oSHA256.ComputeHash($oStream)).Replace("-","").ToLower()
+    $oStream.Close()
+    return($sSHA256)
+}
